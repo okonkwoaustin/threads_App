b1f3629a446045969edcb61949cdfede377452be
Austin Okonkwo <austinokonkwo1995@gmail.com>
2024-10-22 12:24:51 +0100

    chenged the button

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -26,9 +26,9 @@ function Bottombar() {
                             width={24}
                             height={24}
                         />
-                        <p className="text-subtle-medium text-light-1">
+                        <p className="text-subtle-medium text-light-1 max-sm:hidden">
                             {link.label.split(/\s/)[0]}
-                            </p>
+                        </p>
                     </Link>
                 )}
                )} 
